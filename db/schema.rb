5bc69a91703cd4e8022eb64683bb657494222d87
Lo1jke <Lo1jke@gmail.com>
2011-05-04 15:04:33 +0400

    delete fake migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110504103520) do
+ActiveRecord::Schema.define(:version => 20110504094659) do
 
   create_table "comments", :force => true do |t|
     t.string   "name"
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(:version => 20110504103520) do
     t.string   "name"
     t.string   "second_name"
     t.integer  "age"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
   end
 
 end
