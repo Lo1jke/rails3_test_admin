0d8866ad082bdc610a57580530e6a6df6d68d745
Lo1jke <Lo1jke@gmail.com>
2011-05-04 14:39:39 +0400

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3TestAdmin::Application.config.secret_token = '0e44f5321b276640b0e16fda70d165aa30e4af78ea48c8e5e75f366807924c934af4d7954b38afe596b4dc08e64bea970a1ee047f8bf53cbdb6c3379e8af3edb'
---
+++ db/migrate/20110504101836_add_password_to_users.rb
@@ -0,0 +1,12 @@
+class AddPasswordToUsers < ActiveRecord::Migration
+  def self.up
+	change_table :users do |t|
+      t.string :password
+    end
+
+  end
+
+  def self.down
+  remove_column :users, :password
+  end
+end
